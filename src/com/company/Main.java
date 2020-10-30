bf5ce432f90532a66723cdc2bc1f62f16a9d9648
AdrianCiuciui <adrian.ciuciui.cornel@gmail.com>
2020-10-30 07:24:51 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Series series = new Series();
+        series.nSum(10);
+        series.nFactorial(10);
+        series.nFibonacci(10);
+
+    }
+}
